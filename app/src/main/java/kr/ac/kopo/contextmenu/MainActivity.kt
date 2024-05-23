cecc4865de06554badb8b09cdddef57c638b35ce
Byeanseojin <sjb12365@gmail.com>
2024-05-23 15:29:05 +0900

    ContextMenu 완성

---
+++ app/src/main/java/kr/ac/kopo/contextmenu/MainActivity.kt
@@ -0,0 +1,85 @@
+package kr.ac.kopo.contextmenu
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.ContextMenu
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+
+class MainActivity : AppCompatActivity() {
+    lateinit var linear: LinearLayout
+    lateinit var btnLinear: Button
+    lateinit var btnRotation: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        linear = findViewById<LinearLayout>(R.id.linear)
+        btnLinear = findViewById<Button>(R.id.btnLinearBg)
+        btnRotation = findViewById<Button>(R.id.btnRotation)
+
+        registerForContextMenu(btnLinear)
+        registerForContextMenu(btnRotation)
+    }
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+
+        var menuInflater = this.menuInflater
+
+        if (v === btnLinear) {
+            menu!!.setHeaderTitle("배경색 변경")
+            menuInflater.inflate(R.menu.menu1_context, menu)  //위에 menu: ContextMenu?,와 일치
+        }
+        if (v === btnRotation) {
+            menu!!.setHeaderTitle("버튼회전 및 확대")
+            menuInflater.inflate(R.menu.menu2_context, menu)  //위에 menu: ContextMenu?,와 일치
+        }
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        super.onContextItemSelected(item)
+        when(item.itemId){
+            R.id.itemRed -> {
+                linear.setBackgroundColor(Color.RED)
+                return true
+            }
+
+            R.id.itemYellow -> {
+                linear.setBackgroundColor(Color.YELLOW)
+                return true
+            }
+
+            R.id.itemMagenta -> {
+                linear.setBackgroundColor(Color.MAGENTA)
+                return true
+            }
+
+            R.id.itemRotation -> {
+                btnRotation.rotation = 90f
+                return true
+            }
+
+            R.id.itemZoom -> {
+                btnRotation.scaleX = 2f
+                btnRotation.scaleY = 2f
+                return true
+            }
+
+            R.id.itemZoomOriginal -> {
+                btnRotation.scaleX = 1f
+                btnRotation.scaleY = 1f
+                return true
+            }
+        }
+        return false
+    }
+}
